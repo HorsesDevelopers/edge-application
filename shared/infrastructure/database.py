ee73d0fdf99164774aa3421f407821aa37a4554e
Charlie <u201923994@upc.edu.pe>
2025-07-04 11:39:40 -0500

    chore(om): added documentation

--- shared/infrastructure/database.py
+++ shared/infrastructure/database.py
@@ -5,7 +5,7 @@ db = SqliteDatabase('aqua_sense.db')
 
 def init_db() -> None:
     db.connect()
-    from pond.infrastructure.models import PondRecord
+    from om.infrastructure.models import PondRecord
     from om.infrastructure.models import FeedEvent
     from iam.infrastructure.models import Device
     db.create_tables([Device, PondRecord, FeedEvent], safe=True)
